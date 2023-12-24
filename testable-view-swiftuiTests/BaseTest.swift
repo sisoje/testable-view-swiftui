f7c45cd4ecd769b7d3002a384054eabf82d010bf
Lazar Otasevic <>
2023-12-24 23:57:55 +0100

    increase stability of tests

--- testable-view-swiftuiTests/BaseTest.swift
+++ testable-view-swiftuiTests/BaseTest.swift
@@ -12,12 +12,19 @@ import XCTest
 class BaseTest: XCTestCase {
     var cancellables: Set<AnyCancellable> = []
 
-    override func setUp() {
-        ViewinspectorHosting.shared.view = nil
-    }
+    override func setUp() {}
 
     override func tearDown() {
         cancellables.removeAll()
         ViewinspectorHosting.shared.view = nil
+        waitForNextLoop()
+    }
+
+    func waitForNextLoop() {
+        let exp = expectation(description: "wait for next loop")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
